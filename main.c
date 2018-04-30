c3a1a24f3997f28e186607dbb7fc276c81d8a28b
MichaelBlakeOIT <michael.blake@oit.edu>
2018-04-30 12:32:27 -0700

    fixed formatting errors

--- main.c
+++ main.c
@@ -22,54 +22,54 @@ static void * handleRequest(void * request_info);
 
 int main(int argc, char ** argv)
 {
-	int fd = 0;
-	int c;
-	char root[1024] = { 0 }; 
-	struct sockaddr_in client;
-	request_info_t * request_info = 0;
-	pthread_t browser_thread;
+    int fd = 0;
+    int c;
+    char root[1024] = { 0 }; 
+    struct sockaddr_in client;
+    request_info_t * request_info = 0;
+    pthread_t browser_thread;
 
-	if(handleOpts(argc, argv, root) != 0)
-	{
-		return 0;
-	}
+    if(handleOpts(argc, argv, root) != 0)
+    {
+        return 0;
+    }
 
-	printf("Starting server\nRoot Dir: %s\nPort: %d\n", 
-			root, int_port);
+    printf("Starting server\nRoot Dir: %s\nPort: %d\n", 
+            root, int_port);
 
-	fd = establishConnection();
-	c = sizeof(struct sockaddr_in);
+    fd = establishConnection();
+    c = sizeof(struct sockaddr_in);
 
-	while(1)
-	{
-		request_info = malloc(sizeof(request_info_t));		
-		strcpy(request_info->file, root);
-		strcpy(request_info->root, root);
+    while(1)
+    {
+        request_info = malloc(sizeof(request_info_t));        
+        strcpy(request_info->file, root);
+        strcpy(request_info->root, root);
 
-		if((request_info->browser_fd = 
-				accept(fd, (struct sockaddr *)&client, 
-				(socklen_t*)&c)) < 0)
-		{
-			perror("Accept()");
-			free(request_info);
-			close(fd);
-			continue;
-		}
+        if((request_info->browser_fd = 
+                accept(fd, (struct sockaddr *)&client, 
+                (socklen_t*)&c)) < 0)
+        {
+            perror("Accept()");
+            free(request_info);
+            close(fd);
+            continue;
+        }
 
-		if(pthread_create(&browser_thread, NULL, handleRequest, request_info) != 0)
-		{
-			//perror("Pthread_create()");
-			free(request_info);
-			close(fd);
-			continue;
-		}
-		
-		//pthread_detach(browser_thread);
-	}
+        if(pthread_create(&browser_thread, NULL, handleRequest, request_info) != 0)
+        {
+            //perror("Pthread_create()");
+            free(request_info);
+            close(fd);
+            continue;
+        }
+        
+        //pthread_detach(browser_thread);
+    }
 
-	close(fd);
+    close(fd);
 
-	return 0;
+    return 0;
 }
 
 /****************************************************************
@@ -82,27 +82,27 @@ int main(int argc, char ** argv)
 
 void * handleRequest(void * request_info)
 {
-	request_info_t * request_struct = (request_info_t *)request_info;
-	int send_return = 0;
+    request_info_t * request_struct = (request_info_t *)request_info;
+    int send_return = 0;
 
     readRequest(request_struct->browser_fd, 
-				request_struct->file + strlen(request_struct->root));
+                request_struct->file + strlen(request_struct->root));
 
     printf("file: %s\n", request_struct->file);
 
     if((send_return = sendFile(request_struct->browser_fd, 
-							   request_struct->file)) != 0)
+                               request_struct->file)) != 0)
     {
         if(send_return == 1)
         {
             send404(request_struct->browser_fd, request_struct->root);
         }   
     }
-	close(request_struct->browser_fd);
-	free(request_info);
-	pthread_detach(pthread_self());
+    close(request_struct->browser_fd);
+    free(request_info);
+    pthread_detach(pthread_self());
 
-	return 0;
+    return 0;
 }
 
 /****************************************************************
@@ -116,14 +116,14 @@ void * handleRequest(void * request_info)
 
 int establishConnection()
 {
-	int fd = 0;
+    int fd = 0;
     struct sockaddr_in server;
 
     fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd == -1)
     {
         perror("Socket()");
-		exit(1);
+        exit(1);
     }
      
     server.sin_family = AF_INET;
@@ -138,7 +138,7 @@ int establishConnection()
      
     listen(fd , 3);
 
-	return fd;   
+    return fd;   
 }
 
 /****************************************************************
@@ -151,32 +151,32 @@ int establishConnection()
 
 void readRequest(int fd, char * path)
 {
-	int read_size = 0;
-	char line[500] = { 0 };
-	char request[1024] = { 0 };
+    int read_size = 0;
+    char line[500] = { 0 };
+    char request[1024] = { 0 };
 
-	printf("Read Request\n");
+    printf("Read Request\n");
 
-	if((read_size = 
-			recv(fd, request, MAX_REQUEST_SIZE, 0)) < 0)
+    if((read_size = 
+            recv(fd, request, MAX_REQUEST_SIZE, 0)) < 0)
     {
         perror("Recv()");
-		exit(1);
+        exit(1);
     }
 
-	readLine(request, line);
+    readLine(request, line);
 
-	sscanf(line, "GET %s HTTP", path);
+    sscanf(line, "GET %s HTTP", path);
 
-	if(path[strlen(path) - 1] == '/')
-	{
-		strcat(path, "index.html");
-	}
+    if(path[strlen(path) - 1] == '/')
+    {
+        strcat(path, "index.html");
+    }
 
-	/*if(sscanf(path, "/%s?", file) != 1)
-	{
-		strcpy(file, "/index.html");
-	}*/
+    /*if(sscanf(path, "/%s?", file) != 1)
+    {
+        strcpy(file, "/index.html");
+    }*/
 }
 
 /****************************************************************
@@ -189,47 +189,47 @@ void readRequest(int fd, char * path)
 
 int sendFile(int fd, char * file)
 {
-	struct stat st;
-	int bytes_read = 0;
-	char file_buffer[1024] = { 0 };	
-	int file_fd = 0;
-	char file_size[10] = { 0 };
+    struct stat st;
+    int bytes_read = 0;
+    char file_buffer[1024] = { 0 };    
+    int file_fd = 0;
+    char file_size[10] = { 0 };
 
-	printf("Send File\n");
+    printf("Send File\n");
 
-	if((file_fd = open(file, O_RDONLY)) < 0)
-	{
-		return 1;
-	}
-	
-	stat(file, &st);
-	sprintf(file_size, "%ld", st.st_size);
-	memset(file_buffer, 0, 1024);
+    if((file_fd = open(file, O_RDONLY)) < 0)
+    {
+        return 1;
+    }
+    
+    stat(file, &st);
+    sprintf(file_size, "%ld", st.st_size);
+    memset(file_buffer, 0, 1024);
 
-	send(fd, HTTP_VERSION, strlen(HTTP_VERSION), 0);
-	send(fd, OK, strlen(OK), 0);
-	send(fd, CRLF, strlen(CRLF), 0);
-	send(fd, CONTENT_TYPE, strlen(CONTENT_TYPE), 0);
-	send(fd, CRLF, strlen(CRLF), 0);
-	send(fd, CONTENT_LENGTH, strlen(CONTENT_LENGTH), 0);
-	send(fd, file_size, strlen(file_size), 0);
-	send(fd, CRLF, strlen(CRLF), 0);
-	send(fd, CRLF, strlen(CRLF), 0);
+    send(fd, HTTP_VERSION, strlen(HTTP_VERSION), 0);
+    send(fd, OK, strlen(OK), 0);
+    send(fd, CRLF, strlen(CRLF), 0);
+    send(fd, CONTENT_TYPE, strlen(CONTENT_TYPE), 0);
+    send(fd, CRLF, strlen(CRLF), 0);
+    send(fd, CONTENT_LENGTH, strlen(CONTENT_LENGTH), 0);
+    send(fd, file_size, strlen(file_size), 0);
+    send(fd, CRLF, strlen(CRLF), 0);
+    send(fd, CRLF, strlen(CRLF), 0);
 
-	do
-	{
-		bytes_read = read(file_fd, file_buffer, sizeof(file_buffer));
-		if(send(fd, file_buffer, bytes_read, 0) < 0)
-		{
-			perror("Send()");
-			exit(1);
-		}
-		memset(file_buffer, 0, 1024);
-	} while(bytes_read > 0);
+    do
+    {
+        bytes_read = read(file_fd, file_buffer, sizeof(file_buffer));
+        if(send(fd, file_buffer, bytes_read, 0) < 0)
+        {
+            perror("Send()");
+            exit(1);
+        }
+        memset(file_buffer, 0, 1024);
+    } while(bytes_read > 0);
 
-	close(file_fd);
+    close(file_fd);
 
-	return 0;
+    return 0;
 }
 
 /****************************************************************
@@ -255,38 +255,38 @@ void readLine(char * string, char * buffer)
 
 int handleOpts(int argc, char ** argv, char * root)
 {
-	char port[MAX_PORT] = { 0 };
-	int c;
+    char port[MAX_PORT] = { 0 };
+    int c;
 
-	while ((c = getopt (argc, argv, "hp:r:")) != -1)
-	{
-		switch (c)
-		{
-			case 'h':
-				printHelp();
-				return 1;
-				break;
-			case 'p':
-				strncpy(port, optarg, MAX_PORT - 1);
-				sscanf(port, "%hu", &int_port);
-				break;
-			case 'r':
-				strncpy(root, optarg, MAX_ROOT - 1);
-				break;
-			case '?':
-				break;
-			default:
-				printf("Invalid arguments\n");
-				return 1;
-		}
+    while ((c = getopt (argc, argv, "hp:r:")) != -1)
+    {
+        switch (c)
+        {
+            case 'h':
+                printHelp();
+                return 1;
+                break;
+            case 'p':
+                strncpy(port, optarg, MAX_PORT - 1);
+                sscanf(port, "%hu", &int_port);
+                break;
+            case 'r':
+                strncpy(root, optarg, MAX_ROOT - 1);
+                break;
+            case '?':
+                break;
+            default:
+                printf("Invalid arguments\n");
+                return 1;
+        }
 
-		if(root[0] == 0)
-		{
-			printf("Please specify a root directory with -r\n");
-			exit(1);
-		}
-	}
-	return 0;
+        if(root[0] == 0)
+        {
+            printf("Please specify a root directory with -r\n");
+            exit(1);
+        }
+    }
+    return 0;
 }
 
 /****************************************************************
@@ -298,12 +298,12 @@ int handleOpts(int argc, char ** argv, char * root)
 
 void printHelp()
 {
-	printf("Command Line Arguments\n \
-			-h: help\n \
-			-p port (default: 8000): port web server runs on\n \
-			-r dir (required): root directory of website\n");
-	printf("To add a custom 404 page, name the file 404.html \
-			and put it in your website's root directory");
+    printf("Command Line Arguments\n \
+            -h: help\n \
+            -p port (default: 8000): port web server runs on\n \
+            -r dir (required): root directory of website\n");
+    printf("To add a custom 404 page, name the file 404.html \
+            and put it in your website's root directory");
 }
 
 /****************************************************************
@@ -315,18 +315,18 @@ void printHelp()
 
 void send404(int fd, char * root)
 {
-	struct stat st;
+    struct stat st;
     int bytes_read = 0;
     char file_buffer[1024] = { 0 };
     int file_fd = 0;
     char file_size[10] = { 0 };
-	char file[MAX_ROOT] = { 0 };
+    char file[MAX_ROOT] = { 0 };
 
-	strcpy(file, root);
-	strcat(file, "/404.html");
+    strcpy(file, root);
+    strcat(file, "/404.html");
     printf("404: %s\n", file);
 
-	send(fd, HTTP_VERSION, strlen(HTTP_VERSION), 0);
+    send(fd, HTTP_VERSION, strlen(HTTP_VERSION), 0);
     send(fd, NOT_FOUND, strlen(NOT_FOUND), 0);
     send(fd, CRLF, strlen(CRLF), 0);
     send(fd, CONTENT_TYPE, strlen(CONTENT_TYPE), 0);
@@ -339,30 +339,30 @@ void send404(int fd, char * root)
         send(fd, CRLF, strlen(CRLF), 0);
         send(fd, CRLF, strlen(CRLF), 0);
     }
-	else
-	{
-		stat(file, &st);
-		sprintf(file_size, "%ld", st.st_size);
-	    memset(file_buffer, 0, 1024);
+    else
+    {
+        stat(file, &st);
+        sprintf(file_size, "%ld", st.st_size);
+        memset(file_buffer, 0, 1024);
 
-		send(fd, CONTENT_LENGTH, strlen(CONTENT_LENGTH), 0);
-		send(fd, file_size, strlen(file_size), 0);
-		send(fd, CRLF, strlen(CRLF), 0);
-		send(fd, CRLF, strlen(CRLF), 0);
+        send(fd, CONTENT_LENGTH, strlen(CONTENT_LENGTH), 0);
+        send(fd, file_size, strlen(file_size), 0);
+        send(fd, CRLF, strlen(CRLF), 0);
+        send(fd, CRLF, strlen(CRLF), 0);
 
-		do
-		{
-			bytes_read = 
-				read(file_fd, file_buffer, sizeof(file_buffer));
+        do
+        {
+            bytes_read = 
+                read(file_fd, file_buffer, sizeof(file_buffer));
 
-			if(send(fd, file_buffer, bytes_read, 0) < 0)
-			{
-				perror("Send()");
-				exit(1);
-			}
-			memset(file_buffer, 0, 1024);
-		} while(bytes_read > 0);
+            if(send(fd, file_buffer, bytes_read, 0) < 0)
+            {
+                perror("Send()");
+                exit(1);
+            }
+            memset(file_buffer, 0, 1024);
+        } while(bytes_read > 0);
 
-		close(file_fd);
-	}
+        close(file_fd);
+    }
 }
